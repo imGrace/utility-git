758abfa38c47d75fd1a57b1257410b0ad54f4452
liudi <imliudi@gmail.com>
2013-10-04 20:03:47 +0800

    add CData

--- UtilityBox/UtilityBox/main.cpp
+++ UtilityBox/UtilityBox/main.cpp
@@ -8,10 +8,32 @@
 
 #include <iostream>
 #include "CData.h"
-
+void Printf(CData<int> &a)
+{
+    size_t i=0;
+    while (i<a.GetBufSize()) {
+        cout << a[i++] << " ";
+    }
+    cout << endl;
+}
 int main(int argc, const char * argv[])
 {
-    CData<> buf;
+    CData<int> a(5);
+    CData<int> b(13);
+    int d[123] = {1,2,3,4,5,6,8,9,0};
+    
+    cout << sizeof(d)/sizeof(int) << endl;
+    
+    a.append(d, 9);
+    Printf(a);
+    cout << endl;
+    a.append(d, 9);a.append(d, 9);a.append(d, 9);a.append(d, 9);
+    Printf(a);
+    cout << a.GetBufSize() << endl;
+    
+    CData<int> f(100);
+    f.append(a);
+    Printf(f);
     return 0;
 }
 
