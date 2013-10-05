1db6e6d5b3a0f634324e95435cb9155686857f78
liudi <imliudi@gmail.com>
2013-10-05 20:28:21 +0800

    add makeSealed template

---
+++ UtilityBox/UtilityBox/MakeSealed.h
@@ -0,0 +1,34 @@
+//
+//  MakeSealed.h
+//  UtilityBox
+//
+//  Created by Diane on 10/5/13.
+//  Copyright (c) 2013 Diane liudi. All rights reserved.
+//
+
+#ifndef UtilityBox_MakeSealed_h
+#define UtilityBox_MakeSealed_h
+template<typename T>
+class MakeSealed
+{
+    friend T;
+    MakeSealed(){}
+    ~MakeSealed(){}
+};
+
+//virtual is required
+class fun:public virtual MakeSealed<fun>
+{
+public:
+    fun(){}
+    ~fun(){}
+    void sing(){std::cout << "lala" << std::endl;}
+};
+
+//error
+//class fun1:public fun{
+//public:
+//    fun1(){}
+//    ~fun1(){}
+//};
+#endif
--- UtilityBox/UtilityBox/main.cpp
+++ UtilityBox/UtilityBox/main.cpp
@@ -7,33 +7,14 @@
 //
 
 #include <iostream>
-#include "CData.h"
-void Printf(CData<int> &a)
-{
-    size_t i=0;
-    while (i<a.GetBufSize()) {
-        cout << a[i++] << " ";
-    }
-    cout << endl;
-}
+#include "MakeSealed.h"
+
 int main(int argc, const char * argv[])
 {
-    CData<int> a(5);
-    CData<int> b(13);
-    int d[123] = {1,2,3,4,5,6,8,9,0};
-    
-    cout << sizeof(d)/sizeof(int) << endl;
-    
-    a.append(d, 9);
-    Printf(a);
-    cout << endl;
-    a.append(d, 9);a.append(d, 9);a.append(d, 9);a.append(d, 9);
-    Printf(a);
-    cout << a.GetBufSize() << endl;
-    
-    CData<int> f(100);
-    f.append(a);
-    Printf(f);
+    fun a;
+    a.sing();
+    //
+    //fun1 c;
     return 0;
 }
 
